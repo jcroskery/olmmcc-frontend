7d73dcb32224a63ecce72896783bcf6aac9fe11a
Justus Croskery <justus@olmmcc.tk>
2019-02-02 13:34:21 -0500

    Changed home article to new rendering format

--- http/helpers/mainArticle.php
+++ http/helpers/mainArticle.php
@@ -1,9 +1,14 @@
 <?php
+include_once '/srv/http/helpers/wrapper.php';
+include_once '/srv/http/helpers/basicArticle.php';
 class MainArticle extends BasicArticle{
     function __construct($filepath){
         $json = file_get_contents($filepath);
         $article = json_decode($json, true);
-        $text = '<div id="main-text"><H1>' . $title .  '</H1><p>' . $article['text'] . '/<p></div>';
+        $text = '<div id="main-text"><H1>' . $article['title'] .  '</H1><p>' . $article['text'] . '</p></div>';
         parent::__construct($article['title'], $text);
+        wrapperBegin($this->title, $article['topnavId']);
+        echo $this->body;
+        wrapperEnd();
     }
 }
--- http/index.php
+++ http/index.php
@@ -1,11 +1,3 @@
 <?php
-include_once '/srv/http/helpers/wrapper.php';
-wrapperBegin('OLMM Children\'s Choir', 'home');
-echo <<<HTML
-        <div id="main-text">
-            <H1>Home</H1>
-            <p>Hello, and welcome! To read about the choir, visit the About tab. To see the current songs, go to the Songs tab. The choir's schedule is visible in the Calendar tab. You can read the Frequently Asked Questions in the FAQ tab. If you notice a problem in the site or you have questions, go to the Contact tab.
-            </p>
-        </div>
-HTML;
-wrapperEnd();
+include_once '/srv/http/helpers/mainArticle.php';
+new MainArticle('/srv/http/articles/home.json');
