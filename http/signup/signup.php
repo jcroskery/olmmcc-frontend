057853f196b66ad41155ccea683a1f2dcab09bb6
Justus Croskery <justus@olmmcc.tk>
2019-02-16 12:58:35 -0500

    Fixed problems with signup verification

--- http/signup/signup.php
+++ http/signup/signup.php
@@ -7,14 +7,6 @@ $username = sanitizeString($_POST['username']);
 $password1 = sanitizeString($_POST['password']);
 $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 $password2 = sanitizeString($_POST['password2']);
-function passwordMatch()
-{
-    global $password1, $password2;
-    if ($password1 == $password2) {return true;}
-    $message = "Sorry, your passwords do not match. Please try again.";
-    displayPopupNotification($message, '/signup/');
-    return false;
-}
 function checkEmail()
 {
     global $email;
@@ -32,28 +24,32 @@ function checkEmail()
 function checkUsername()
 {
     global $username;
-    if (strlen($username) <= 16 && preg_match('/[A-Za-z0-9]/', $username)) {
+    if (strlen($username) <= 16 && strlen($username) >= 4 && !preg_match('/[^A-Za-z0-9]/', $username)) {
         if (!getAccountFromEmail($email)) {
             return true;
         }
         $message = "Sorry, this username has already been taken. Please select another.";
     } else {
-        $message = 'Sorry, your username is invalid. Please select a different username.';
+        $message = 'Sorry, your username is invalid. Please use between 4 and 16 characters and only letters and numbers for your username.';
     }
     displayPopupNotification($message, '/signup/');
     return false;
 }
 function checkPassword()
 {
-    global $password;
-    if (strlen($password) <= 128 && strlen($password) >= 8) {return true;}
-    $message = 'Please use a password between 8 and 128 characters long.';
+    global $password1, $password2;
+    if (strlen($password1) <= 128 && strlen($password1) >= 8) {
+        if ($password1 == $password2) {return true;}
+        $message = "Sorry, your passwords do not match. Please try again.";
+    } else {
+        $message = 'Please use a password between 8 and 128 characters long.';
+    }
     displayPopupNotification($message, '/signup/');
     return false;
 }
-if (passwordMatch() && checkEmail() && checkUsername() && checkPassword()) {
+if (checkEmail() && checkUsername() && checkPassword()) {
     createAccount($email, $username, $hashedPassword);
     session_unset();
-    $message = "Congratulations! Your account was sucessfully created. Please login with your email and password.";
+    $message = "Your account was sucessfully created! Please login to your new account.";
     displayPopupNotification($message, '/login/', false);
 }
