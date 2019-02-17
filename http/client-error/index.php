b08587adfbab7dfd4d32cf968f4e2f0ae0c172a3
Justus Croskery <justus@olmmcc.tk>
2019-02-17 15:22:31 -0500

    Changed 400-series error message to notification, moved error file

---
+++ http/client-error/index.php
@@ -0,0 +1,3 @@
+<?php
+include_once '/srv/http/helpers/displayMessage.php';
+displayPopupNotification('A 400 series error has occurred. Most likely you are not allowed to view this page or the page does not exist.', '/');
