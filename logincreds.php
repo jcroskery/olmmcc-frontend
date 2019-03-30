0008446a61b67150a3973e5da83b682a2ab82482
Justus Croskery <justus@olmmcc.tk>
2019-03-30 08:52:28 -0400

    Updated gitignore

--- logincreds.php
+++ logincreds.php
@@ -16,7 +16,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program. If not, see https://www.gnu.org/licenses/.
 */
-include_once 'privateVars.php';
+include_once '/etc/httpd/privateVars.php';
 function sanitizeString($var)
 {
     if (get_magic_quotes_gpc) {
