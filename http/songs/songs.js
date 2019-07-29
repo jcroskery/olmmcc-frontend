81377ded96bf2bc591ccc2f25beb18bac663dc9f
Justus Croskery <justus@olmmcc.tk>
2019-07-29 15:36:24 -0400

    Changed songs.js to reflect rust backend

--- http/songs/songs.js
+++ http/songs/songs.js
@@ -31,7 +31,11 @@ function display(currentId) {
     let div = document.createElement('div');
     let iframe = document.createElement('iframe');
     iframe.className = 'video';
-    iframe.src = parsedResponse.songs[currentId].link;
+    for(song in parsedResponse.songs) {
+        if(song.name = currentId) {
+            iframe.src = song.link;
+        }
+    }
     iframe.frameBorder = 0;
     iframe.allowFullscreen = true;
     div.appendChild(iframe);
@@ -58,13 +62,13 @@ function displaySongLinks() {
         p.textContent = parsedResponse.text;
         document.getElementById('main-text').appendChild(h3);
         document.getElementById('main-text').appendChild(p);
-        for (id in parsedResponse.songs){
+        for (song in parsedResponse.songs){
             let h4 = document.createElement('h4');
-            h4.textContent = parsedResponse.songs[id].role + ": ";
+            h4.textContent = song.role + ": ";
             let a = document.createElement('a');
             a.href = 'javascript:;';
-            a.id = id;
-            a.innerText = parsedResponse.songs[id].name;
+            a.id = song.name;
+            a.innerText = song.name;
             a.addEventListener('click', displayPopupVideo);
             h4.appendChild(a);
             document.getElementById('main-text').appendChild(h4);
@@ -76,5 +80,5 @@ function displaySongLinks() {
     }
 }
 {
-    submitXHR(new FormData(), "/api/songs/getSongs.php", displaySongLinks);
+    submitXHR(new FormData(), "https://api.olmmcc.tk/get_songs", displaySongLinks);
 }
