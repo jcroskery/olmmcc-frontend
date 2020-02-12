e649964cbc4bbff3d628ed2edd8a85acab494afc
Justus Croskery <justus@olmmcc.tk>
2020-02-12 09:29:37 -0500

    Change signup to not require password

--- http/signup/signup.js
+++ http/signup/signup.js
@@ -16,22 +16,28 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see https://www.gnu.org/licenses/.
  */
 function handleSignup(json) {
-    if (!json.success) {
-        createNotification("An error occurred. Please try again.");
+    if (json.message) {
+        createNotification(json.message);
     } else {
-        window.localStorage.setItem("notification", "Your account was successfully created! Please log in to your account.");
-        window.location = "/login";
+        window.localStorage.setItem("unverified_session", json.session);
+        let formData = new FormData();
+        formData.append("session", json.session);
+        sendReq(formData, 'https://api.olmmcc.tk/send_verification_email', (json) => {
+            if (json.success == true) {
+                window.localStorage.setItem("notification", "An verification code has been sent to your email at " + email + ". Please check your inbox and spam folder. If you do not receive the email then log in again.");
+                window.location = "/account/verify";
+            }
+        });
     }
 }
 function submitSignup() {
     let formData = new FormData();
-    formData.append('email', document.getElementById('email').value);
-    formData.append('password1', document.getElementById('password1').value);
-    formData.append('password2', document.getElementById('password2').value);
+    email = document.getElementById('email').value
+    formData.append('email', email);
     sendReq(formData, 'https://api.olmmcc.tk/signup', handleSignup);
 }
 document.getElementById('signup').addEventListener('click', submitSignup);
-document.getElementById('password2').addEventListener('keydown', (event) => {
+document.getElementById('email').addEventListener('keydown', (event) => {
     if (event.code === 'Enter') {
         submitSignup();
     }
