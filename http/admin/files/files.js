bbb49db48b81b8e800124fdd27801030d24f0a79
Justus Croskery <justus@olmmcc.tk>
2020-03-03 15:16:07 -0500

    Change layout of songs

--- http/admin/files/files.js
+++ http/admin/files/files.js
@@ -1,5 +1,28 @@
 let audio_created = false;
-const displaySong = () => {
+const lista = [
+    ["Sonatine", 1], 
+    ["The Infant Paganini", 2], 
+    ["Concertino in the Style of Vivaldi", 3],
+    ["Concertino in the Style of Mozart", 4],
+    ["Concerto in B Minor", 5]
+];
+const listb = [
+    ["Sonata in F Major", 6],
+    ["Sarabande", 7],
+    ["Giguetta", 8], 
+    ["Bourree", 9]
+];
+const listc = [
+    ["The Song of Twilight", 10],
+    ["Soldatenmarsch", 11],
+    ["The Faun", 12],
+    ["Summer Song", 13],
+    ["Merry-Go-Round", 14],
+    ["Neapolitan Dance", 15],
+    ["Knight Rupert", 16],
+    ["Fast Dance", 17]
+];
+function changeSong(input) {
     let audio;
     if (!audio_created) {
         let br = document.createElement("br");
@@ -14,13 +37,55 @@ const displaySong = () => {
     } else {
         audio = document.getElementById("audio");
     }
-    let input = document.getElementById("song").value;
     audio.src = "Track " + input + ".mp3";
-};
-let song = document.getElementById("song");
-song.addEventListener("change", displaySong);
-window.addEventListener("keypress", (ev) => {
-    if(ev.key == "Enter") {
-        displaySong()
+}
+
+function clearList() {
+    let list = document.getElementById("song");
+    while (list.firstChild) {
+        list.removeChild(list.lastChild);
     }
-});
+}
+function setList(list) {
+    let songs = document.getElementById("song");
+    for(let i = 0; i < list.length; i++) {
+        let option = document.createElement("option");
+        if (document.getElementById("instruments").value == "piano") {
+            option.value = list[i][1] + 17;
+        } else {
+            option.value = list[i][1];
+        }
+        option.textContent = list[i][0];
+        songs.appendChild(option);
+    }
+}
+function changeList(value) {
+    switch(value) {
+        case 'a': {
+            clearList();
+            setList(lista);
+            break;
+        }
+        case 'b': {
+            clearList();
+            setList(listb);
+            break;
+        }
+        case 'c': {
+            clearList();
+            setList(listc);
+            break;
+        }
+    }
+    changeSong(document.getElementById("song").value)
+}
+document.getElementById("list").onchange = (e) => {
+    changeList(e.target.value);
+};
+changeList(document.getElementById("list").value);
+document.getElementById("song").onchange = (e) => {
+    changeSong(e.target.value);
+}
+document.getElementById("instruments").onchange = () => {
+    changeList(document.getElementById("list").value);
+}
